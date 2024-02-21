7bd6019d5b225cf102e19318e53b04feebfe3ffd
0MATRIX0 <bellamsujith4444666@gmail.com>
2024-02-21 00:36:45 -0500

    Add new screens and remove old screens

---
+++ screens/ContentCalendar.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ContentCalendar = () => {
+    return (
+        <View>
+            <Text>ContentCalendar</Text>
+        </View>
+    )
+}
+
+export default ContentCalendar
---
+++ screens/MyBrand.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MyBrand = () => {
+  return (
+    <View>
+      <Text>MyBrand</Text>
+    </View>
+  );
+};
+
+export default MyBrand;
