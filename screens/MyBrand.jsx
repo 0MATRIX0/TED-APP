f2921a48c3f361699d816e821ddc95d77f67a36b
0MATRIX0 <bellamsujith4444666@gmail.com>
2024-03-06 21:53:31 -0500

    Add loading state and fetch brands from API in MyBrand component

--- screens/MyBrand.jsx
+++ screens/MyBrand.jsx
@@ -1,11 +1,53 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ActivityIndicator } from "react-native";
+import React, { useState, useEffect } from "react";
+import { FIREBASE_AUTH } from "../FirebaseConfig";
+import CreateBrandError from "./CreateBrandError";
 
 const MyBrand = () => {
+  const [loading, setLoading] = useState(true);
+  const [brands, setBrands] = useState([]);
+
+  useEffect(() => {
+    const user = FIREBASE_AUTH.currentUser;
+    const userId = user ? user.uid : null;
+
+    fetch("http://192.168.40.234:5000/api/brands/get-brands", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userId: userId,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data?.brands) setBrands(data?.brands);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, [FIREBASE_AUTH.currentUser]);
+
   return (
-    <View>
-      <Text>MyBrand</Text>
-    </View>
+    <>
+      {loading ? (
+        <ActivityIndicator size="large" color="#4fd216" />
+      ) : (
+        <>
+          {brands.length > 0 ? (
+            <>
+              <View>
+                <Text>ContentCalendar</Text>
+              </View>
+            </>
+          ) : (
+            <CreateBrandError />
+          )}
+        </>
+      )}
+    </>
   );
 };
 
