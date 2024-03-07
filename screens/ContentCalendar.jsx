17abc7f519dff171d9d51ae9c41a84bdffce9a8d
0MATRIX0 <bellamsujith4444666@gmail.com>
2024-03-06 19:52:15 -0500

    Refactor ContentCalendar component to fetch brands from API

--- screens/ContentCalendar.jsx
+++ screens/ContentCalendar.jsx
@@ -1,12 +1,54 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { FIREBASE_AUTH } from "../FirebaseConfig";
+import CreateBrandError from "./CreateBrandError";
 
 const ContentCalendar = () => {
-    return (
-        <View>
-            <Text>ContentCalendar</Text>
-        </View>
-    )
-}
+  const [loading, setLoading] = useState(true);
+  const [brands, setBrands] = useState([]);
 
-export default ContentCalendar
+  useEffect(() => {
+    const user = FIREBASE_AUTH.currentUser;
+    const userId = user ? user.uid : null;
+
+    fetch("http://192.168.40.234:5000/api/brands/get-brands", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userId: userId,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data?.brands) setBrands(data?.brands);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, [FIREBASE_AUTH.currentUser]);
+
+  return (
+    <>
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <>
+          {brands.length > 0 ? (
+            <>
+              <View>
+                <Text>ContentCalendar</Text>
+              </View>
+            </>
+          ) : (
+            <CreateBrandError />
+          )}
+        </>
+      )}
+    </>
+  );
+};
+
+export default ContentCalendar;
