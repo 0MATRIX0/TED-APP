2648e108b7c56c5f5f7ae14279de117d689a7368
matrix <bellamsujith4444666@gmail.com>
2024-03-10 16:47:47 -0400

    Add brand animation and update API endpoint

--- components/BrandAnimation.jsx
+++ components/BrandAnimation.jsx
@@ -1,10 +1,45 @@
+import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
-import React from "react";
+import LottieView from "lottie-react-native";
+import { FIREBASE_AUTH } from "../FirebaseConfig";
+import { useRoute } from "@react-navigation/native";
 
 const BrandAnimation = () => {
+  const route = useRoute();
+  const brandId = route.params?.brandId;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://192.168.40.60:4000/api/brands/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userId: FIREBASE_AUTH.currentUser.uid, brandId }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <View>
-      <Text>BrandAnimation</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      {loading ? (
+        <LottieView
+          source={require("../assets/lottie/brandAnimation.json")} // Replace with the path to your Lottie file
+          autoPlay
+          loop
+          style={{ width: 200, height: 200 }} // Adjust size as needed
+        />
+      ) : (
+        <Text>Animation Finished</Text> // Display this text once the loading is complete
+      )}
     </View>
   );
 };
